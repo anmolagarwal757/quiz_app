1b6ae96a9cd98fb7a2c520455f4d3dba6f5933fe
satyamgupta1495 <satyamgupta1495@gmail.com>
2021-11-30 14:09:35 +0530

    added timer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
   const questions = [
@@ -30,6 +30,15 @@ export default function App() {
       ],
     },
     {
+      questionText: "The iPhone was created by which company?",
+      answerOptions: [
+        { answerText: "Apple", isCorrect: 10 },
+        { answerText: "Intel", isCorrect: 2 },
+        { answerText: "Amazon", isCorrect: 1 },
+        { answerText: "Microsoft", isCorrect: 2 },
+      ],
+    },
+    {
       questionText: "How many Harry Potter books are there?",
       answerOptions: [
         { answerText: "1", isCorrect: 3 },
@@ -45,16 +54,53 @@ export default function App() {
   const [score, setScore] = useState(0);
   const [questionNo, setQuestionNo] = useState(1);
 
-  const handleAnswerButtonClick = (isCorrect) => {
+  const [timer, setTimer] = useState(5);
+
+  useEffect(() => {
+    timeOut();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timer]);
+
+  // function resetTimer() {
+  //   setTimer(3);
+  //   nextQuestion();
+  // }
+
+  var setTime;
+  const timeOut = () => {
+    setTime = setTimeout(() => {
+      if (timer === 0) {
+        resetTimer();
+      } else {
+        setTimer(timer - 1);
+      }
+    }, 1000);
+  };
+
+  function resetTimer() {
+    if (clearTimeout(setTime)) {
+      console.log("success");
+    }
+    timeOut();
+    setTimer(5);
+    nextQuestion();
+  }
+
+  const nextQuestion = () => {
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
       setshowScore(true);
     }
-
     setQuestionNo(questionNo + 1);
+  };
 
+  const handleAnswerButtonClick = (isCorrect) => {
+    resetTimer();
+
+    nextQuestion();
+    setQuestionNo(questionNo + 1);
     let trustScore = score + isCorrect;
     setScore(trustScore);
   };
@@ -63,26 +109,35 @@ export default function App() {
     <div className="app">
       {}
       {showScore ? (
-        <div className="score-section">Your Trust Score is: {score}</div>
+        <div className="score-section">
+          Your Trust Score is: {score} / {questions.length * 10}{" "}
+        </div>
       ) : (
         <>
-          <div className="question-section">
-            <div className="question-count">
-              <span>Question {questionNo}</span>/{questions.length}
+          <div className="timer">{timer}</div>
+          <div className="question-wrapper">
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {questionNo}</span>/{questions.length}
+              </div>
+              <div className="question-text">
+                {questions[currentQuestion].questionText}
+              </div>
             </div>
-            <div className="question-text">
-              {questions[currentQuestion].questionText}
+            <div className="answer-section">
+              {questions[currentQuestion].answerOptions.map((answerOptions) => (
+                <button
+                  onClick={() => {
+                    handleAnswerButtonClick(answerOptions.isCorrect);
+                    timeOut();
+                  }}
+                >
+                  {answerOptions.answerText}
+                </button>
+              ))}
             </div>
           </div>
-          <div className="answer-section">
-            {questions[currentQuestion].answerOptions.map((answerOptions) => (
-              <button
-                onClick={() => handleAnswerButtonClick(answerOptions.isCorrect)}
-              >
-                {answerOptions.answerText}
-              </button>
-            ))}
-          </div>
+          <div>{/* <button onClick={onClickResetButton}>Reset</button> */}</div>
         </>
       )}
     </div>
---
+++ src/test.js
@@ -0,0 +1,8 @@
+let a = setTimeout(() => {
+    hello();
+
+}, 1000)
+
+function hello() {
+    console.log("hello world");
+}
